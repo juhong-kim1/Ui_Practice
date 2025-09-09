b8542e368dd7096b7484e20b229b937e64f2b040
김주홍 <gicente1@nate.com>
2025-09-09 17:55:02 +0900

    addPackage

---
+++ File UI Examples (4)/Assets/Sample.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    public Transform panel;
+
+    public Transform scrollView;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            panel.SetAsLastSibling();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            scrollView.SetAsLastSibling();
+        }
+    }
+}
